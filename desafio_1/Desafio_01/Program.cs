2c24c60e934d5ee35596a3cd0864ba608efc3bd2
Lucas Coimbra <lucascoimbralago@gmail.com>
2023-05-05 08:31:26 -0300

    Compactador e Descompactador de byte

---
+++ desafio_1/Desafio_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio_01
+{
+    class Program {
+
+        byte[] compact(byte[] originalData)
+        {
+            byte[] vetorCompactado = new byte[originalData.Length / 2];
+            int j = 0;
+
+            for (int i = 0; i < originalData.Length; i += 2 )
+            {
+                vetorCompactado[j] = (byte)(originalData[i] & 240 | originalData[i + 1] >> 4);
+                j++;
+            }
+            return vetorCompactado
+        }
+        byte[] decompact(byte[] compactedData)
+        {
+            byte[] descompactarVetor = new byte[compactedData.Length]
+            int k = 0;
+
+            for (int i = 0; i < compactedData.Length)
+            {
+                descompactarVetor[k] = (byte)(240 & compactedData[i];
+                descompactarVetor[k+1] = (byte)(compactedData[i] << 4)
+                k += 2;
+            }
+            return descompactarVetor
+        }
+    }
+}
+
