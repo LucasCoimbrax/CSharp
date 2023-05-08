f76b944c9c16571a161426af34973660dd634099
Lucas Coimbra <lucascoimbralago@gmail.com>
2023-05-08 08:54:10 -0300

    Exercicio 1

---
+++ Exercicios/exercicio_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Exercicio 1
+
+using System;
+using static System.Console;
+
+string InverterString(string str)
+{
+    int tamanho = str.Length;
+    
+    char[] caracteres = new char[tamanho];
+    
+    for (int i = 0; i < tamanho; i++)
+    {
+        caracteres[i] = str[tamanho - 1 - i];
+    }
+    
+    return new string(caracteres);
+}
+
+string valor = "";
+dynamic valores = "";
+
+int numberInput = int.Parse(ReadLine());
+
+int numero = Convert.ToInt32(numberInput);
+
+if( numero == 0 || numero == 1 ){
+    return valores = Convert.ToString(numero);
+}else{
+    while (numero > 0){
+        valor += Convert.ToString(numero % 2);
+        numero = numero / 2;
+    }
+    return InverterString(valor);
+};
